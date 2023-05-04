f5cab1f5114f1ca36641b0f4ffa5eb37ef119f6d
Daria <dlopezf@ulasalle.edu.pe>
2023-05-03 23:32:16 -0500

    Avance de Horario

---
+++ backend/blueprints/horarios_blueprint.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from werkzeug.utils import secure_filename
+import json
+from flask_cors import CORS, cross_origin 
+
+from backend.models.post_salones_model import HorarioModel
+model = HorarioModel()
+
+
+horarios_blueprint = Blueprint('horarios_blueprint', __name__)
+
+
+@horarios_blueprint.route('/horario', methods=['PUT'])
+@cross_origin()
+def create_horario():
+    content = model.create_horario(request.json['hora_inicio'], request.json['hora_fin'], request.json['dia'])    
+    return jsonify(content)
+
+@horarios_blueprint.route('/horario', methods=['PATCH'])
+@cross_origin()
+def update_horario():
+    content = model.update_horario(request.json['codigo'], request.json['hora_inicio'], request.json['hora_fin'], request.json['dia'])    
+    return jsonify(content)
+
+@horarios_blueprint.route('/horario', methods=['DELETE'])
+@cross_origin()
+def delete_horario():
+    return jsonify(model.delete_horario(request.json['codigo']))
+
+@horarios_blueprint.route('/horario', methods=['POST'])
+@cross_origin()
+def get_horario():
+    return jsonify(model.get_horario(request.json['codigo']))
+
+@horarios_blueprint.route('/horarios', methods=['POST'])
+@cross_origin()
+def get_horarios():
+    return jsonify(model.get_horarios())
---
+++ backend/models/post_horarios_model.py
@@ -0,0 +1,58 @@
+from backend.models.post_connection_pool import PostgreSQLPool
+
+class HorarioModel:
+    def __init__(self):        
+        self.post_pool = PostgreSQLPool()
+
+    def get_horario(self, horario_codigo):    
+        params = {'codigo' : horario_codigo}      
+        rv = self.post_pool.execute("SELECT * from horarios where codigo=%(codigo)s", params)                
+        data = []
+        content = {}
+        for result in rv:
+            content = {'codigo': result[0], 'hora_inicio': result[1], 'hora_fin': result[2], 'dia': result[3]}
+            data.append(content)
+            content = {}
+        return data
+
+    def get_horarios(self):  
+        rv = self.post_pool.execute("SELECT * from horarios")  
+        data = []
+        content = {}
+        for result in rv:
+            content = {'codigo': result[0], 'hora_inicio': result[1], 'hora_fin': result[2], 'dia': result[3]}
+            data.append(content)
+            content = {}
+        return data
+
+    def create_horario(self, hora_inicio, hora_fin, dia):    
+        data = {
+            'hora_inicio': hora_inicio,
+            'hora_fin': hora_fin,
+            'dia': dia
+        }  
+        query = """insert into horarios (pabelhora_iniciolon, hora_fin, dia) 
+            values ( %(hora_inicio)s, %(hora_fin)s, %(dia)s)"""    
+        cursor = self.post_pool.execute(query, data, commit=True)   
+        return data
+
+    def update_horario(self, codigo, hora_inicio, hora_fin, dia):    
+        data = {
+            'codigo' : codigo,
+            'hora_inicio': hora_inicio,
+            'hora_fin': hora_fin,
+            'dia': dia
+        }  
+        query = """update horarios set hora_inicio = %(hora_inicio)s, hora_fin = %(hora_fin)s, dia = %(dia)s where codigo = %(codigo)s"""    
+        cursor = self.post_pool.execute(query, data, commit=True)   
+
+        result = {'result':1} 
+        return result
+
+    def delete_horario(self, codigo):    
+        params = {'codigo' : codigo}      
+        query = """delete from horarios where codigo = %(codigo)s"""    
+        self.post_pool.execute(query, params, commit=True)   
+
+        result = {'result': 1}
+        return result 
